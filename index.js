8ee728f8c9962cf671adfbd9561af98551147619
Rajat <rajatlko13@gmail.com>
2022-10-19 02:03:11 +0530

    route added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(cors());
 require('dotenv').config();
 
+const routes = require('./routes/routes');
+
 // const port = process.env.DB_PORT || 32221;
 const port = 32221;
 
@@ -14,6 +16,8 @@ app.get('/test', async (req,res) => {
 app.get('/testing', async (req,res) => {
     res.send("Welcome to testing API!!!");  //added for testing
 });
+
+app.use('/api',routes);
 
 app.listen(port, function(err) {
     if(err) {
